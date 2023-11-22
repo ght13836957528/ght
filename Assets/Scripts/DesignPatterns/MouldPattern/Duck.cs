66c0ad603fabff68b41318d2ed46833b455ad78d
Baiji <13836957528@163.com>
2023-11-22 21:04:51 +0800

    [MyProject]  - 提交模板模式 钩子使用

---
+++ Assets/Scripts/DesignPatterns/MouldPattern/Duck.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace DesignPatterns.MouldPattern
+{
+    public class Duck : IComparable
+    {
+        private string _name;
+        private float _weight;
+
+        public Duck(string name, float weight)
+        {
+            _name = name;
+            _weight = weight;
+        }
+
+        /// <summary>
+        /// 钩子使用
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public int CompareTo(object obj) 
+        {
+            Duck duck = (Duck)obj;
+            int result = 1;
+            if (_weight > duck._weight)
+                result = -1;
+            else if (_weight.Equals(duck._weight))
+                result = 0;
+            else if (_weight < duck._weight)
+                result = 1;
+            return result;
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public float GetWeight()
+        {
+            return _weight;
+        }
+    }
+}
--- Assets/Scripts/DesignPatterns/MouldPattern/MouldEntrance.cs
+++ Assets/Scripts/DesignPatterns/MouldPattern/MouldEntrance.cs
@@ -3,11 +3,29 @@ using UnityEngine;
 
 namespace DesignPatterns.MouldPattern
 {
+    /// <summary>
+    /// 模板模式测试入口类
+    /// </summary>
     public class MouldEntrance : MonoBehaviour
     {
         private void Start()
         {
             CoffeePrepare coffeePrepare = new CoffeePrepare();
+
+            Duck[] ducks =
+            {
+                new Duck("aaa", 5),
+                new Duck("bbb", 2.3f),
+                new Duck("cc", 10),
+                new Duck("uuu", 7.0f),
+            };
+            
+            Array.Sort(ducks);
+
+            foreach (var duck in ducks)
+            {
+                Debug.Log("duck name==" + duck.GetName() + " weight===" + duck.GetWeight());
+            }
         }
     }
 }
